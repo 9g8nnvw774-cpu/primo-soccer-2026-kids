7f27132ddf96c2ed1a7f5f161376d73c3f2a5897
9g8nnvw774-cpu <9g8nnvw774@privaterelay.appleid.com>
2026-06-04 23:47:46 -0300

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+window.PRIMO_SUPABASE_CONFIG = {
+  url: "https://beaudafzwragumrqgkhu.supabase.co",
+  anonKey: "sb_publishable_gJ0YRZ6hkjbcBx9ImDeowQ_3zsIb3vj",
+  appId: "primo_soccer_2026_kids"
+};
